e33d5b32a8885382f5d37af84024f9720951e679
Axinite <khe1@student.unimelb.edu.au>
2016-10-16 22:07:33 +1100

    Algorithm

--- app/src/main/java/com/snapchat/team2/snapchat/networkService/DiscoverDataService.java
+++ app/src/main/java/com/snapchat/team2/snapchat/networkService/DiscoverDataService.java
@@ -20,6 +20,7 @@ import com.snapchat.team2.snapchat.customAdapter.SubStoryDerpAdapter;
 import com.snapchat.team2.snapchat.dbHelper.DBManager;
 import com.snapchat.team2.snapchat.dbService.UserDBService;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -43,6 +44,10 @@ public class DiscoverDataService {
 
         String requestURL_base = fragment.getActivity().getResources().getString(R.string.serverAddress) + requestURL;
 
+        String news=null;
+        String tech=null;
+        String bussiness=null;
+
         String user_id;
         SharedPreferences shared = fragment.getActivity().getSharedPreferences("snapchat_user", fragment.getActivity().MODE_PRIVATE);
         user_id=shared.getString("user_id", null);
@@ -50,20 +55,64 @@ public class DiscoverDataService {
         UserDBService userDBService = new UserDBService(DBManager.getInstance(fragment.getActivity()));
         String[] clicks = userDBService.getClicks(user_id);
 
+        int newsDig = (Integer.parseInt(clicks[0]));
+        int techDig =(Integer.parseInt(clicks[1]));
+        int bussinessDig = (Integer.parseInt(clicks[2]));
 
-        final String news=clicks[0];
-        final String tech =clicks[1];
-        final String bussiness=clicks[2];
+
+        System.out.println("HereHereHereHereHereHereHereHere---Before");
+        System.out.println(clicks[0]);
+        System.out.println(clicks[1]);
+        System.out.println(clicks[2]);
+
+        int numb[] = {newsDig,techDig,bussinessDig};
+
+        Arrays.sort(numb);
+
+        if(newsDig==numb[0]){
+            news = "1";
+        }else if(techDig==numb[0]){
+            tech = "1";
+        }else{
+            bussiness = "1";
+        }
+
+
+        if(newsDig==numb[1]){
+            news = "2";
+        }else if(techDig==numb[1]){
+            tech = "2";
+        }else{
+            bussiness = "2";
+        }
+
+        if(newsDig==numb[2]){
+            news = "3";
+        }else if(techDig==numb[2]){
+            tech = "3";
+        }else{
+            bussiness = "3";
+        }
+
+        System.out.println("HereHereHereHereHereHereHereHere---After");
+        System.out.println(news);
+        System.out.println(tech);
+        System.out.println(bussiness);
+
+
+//        final String news=clicks[0];
+//        final String tech =clicks[1];
+//        final String bussiness=clicks[2];
 
 //        final String news="2";
 //        final String tech ="2";
 //        final String bussiness="2";
 
+        final String tech_value = tech;
+        final String news_value = news;
+        final String bussiness_value = bussiness;
 
-        System.out.println("HereHereHereHereHereHereHereHere");
-        System.out.println(clicks[0]);
-        System.out.println(clicks[1]);
-        System.out.println(clicks[2]);
+
 
 
         StringRequest stringRequest = new StringRequest(Request.Method.POST, requestURL_base,
@@ -109,9 +158,9 @@ public class DiscoverDataService {
             protected Map<String, String> getParams() {
                 // setting post
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("tech", tech);
-                params.put("news", news);
-                params.put("bussiness", bussiness);
+                params.put("tech", tech_value);
+                params.put("news", news_value);
+                params.put("bussiness", bussiness_value);
                 return params;
             }
         };
